e7296bcf128bef6720362a2dfcd4c8cfec1a499f
Amirabbas Fazelinia <utilyre@proton.me>
2023-04-24 21:30:55 +0330

    feat: implement basic instantiation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,42 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::fmt::{self, Display, Formatter};
+
+pub struct Matrix<T> {
+    row: usize,
+    col: usize,
+    elements: Vec<T>,
+}
+
+impl<T> Display for Matrix<T>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        for i in 0..self.row {
+            write!(f, "|")?;
+            for j in 0..self.col {
+                write!(f, "{:<4}", self.elements[i * self.col + j])?;
+            }
+            write!(f, "|")?;
+
+            writeln!(f)?;
+        }
+
+        Ok(())
+    }
+}
+
+impl<T> Matrix<T> {
+    pub fn new(row: usize, col: usize, elements: Vec<T>) -> Self {
+        assert_eq!(
+            elements.len(),
+            row * col,
+            "expected {} elements but only got {}",
+            row * col,
+            elements.len()
+        );
+
+        Self { row, col, elements }
+    }
 }
 
 #[cfg(test)]
