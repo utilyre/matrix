a881c033c881b079c41ea7040ee6cf0c23743868
Amirabbas Fazelinia <utilyre@proton.me>
2023-04-26 22:01:26 +0330

    feat(matrix): implement iter(), second try

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,57 @@
-#[derive(Clone)]
-pub struct Cursor {
-    width: usize,
+use std::slice::Iter;
 
-    pub i: usize,
-    pub j: usize,
+#[derive(Clone)]
+pub struct Cursor<I> {
+    iter: I,
+
+    width: usize,
+    i: usize,
+    j: usize,
 }
 
-impl Cursor {
-    fn new(width: usize) -> Self {
-        Self { width, i: 0, j: 0 }
+impl<I> Cursor<I> {
+    fn new(iter: I, width: usize) -> Self {
+        Self {
+            iter,
+            width,
+            i: 0,
+            j: 0,
+        }
     }
+}
 
-    fn tick(&mut self) -> usize {
+impl<I> Iterator for Cursor<I>
+where
+    I: Iterator,
+{
+    type Item = ((usize, usize), I::Item);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let a = self.iter.next();
+
+        let i = self.i;
+        let j = self.j;
+
         self.j += 1;
         if self.j == self.width {
             self.i += 1;
             self.j = 0;
         }
 
-        self.i * self.width + self.j
+        a.map(|a| ((i, j), a))
     }
 }
 
-pub struct MatrixIter<'a, T> {
-    elements: &'a [T],
-    cursor: Cursor,
-}
-
-impl<'a, T> Iterator for MatrixIter<'a, T> {
-    type Item = (Cursor, &'a T);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let cursor = self.cursor.clone();
-        self.elements
-            .get(self.cursor.tick() - 1)
-            .map(|element| (cursor, element))
+trait CursorIter<T> {
+    fn cursor(self, width: usize) -> Cursor<Self>
+    where
+        Self: Sized + Iterator<Item = T>,
+    {
+        Cursor::new(self, width)
     }
 }
+
+impl<I, T> CursorIter<T> for I where I: Iterator<Item = T> {}
 
 pub struct Matrix<T> {
     width: usize,
@@ -48,11 +63,8 @@ impl<T> Matrix<T> {
         Self { width, elements }
     }
 
-    pub fn iter(&self) -> MatrixIter<T> {
-        MatrixIter {
-            elements: &self.elements,
-            cursor: Cursor::new(self.width),
-        }
+    pub fn iter(&self) -> Cursor<Iter<T>> {
+        self.elements.iter().cursor(self.width)
     }
 }
 
@@ -61,7 +73,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {
-        let matrix = Matrix::new(3, vec![1, 2, 3, 4, 5, 6]);
+    fn iteration() {
+        let matrix = Matrix::new(2, vec![1, 2, 3, 4]);
+
+        let mut iterator = matrix.iter();
+        assert_eq!(iterator.next(), Some(((0, 0), &1)));
+        assert_eq!(iterator.next(), Some(((0, 1), &2)));
+        assert_eq!(iterator.next(), Some(((1, 0), &3)));
+        assert_eq!(iterator.next(), Some(((1, 1), &4)));
     }
 }
