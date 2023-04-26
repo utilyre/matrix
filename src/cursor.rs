79808317e4021e07550de61d6992979495c8d462
Amirabbas Fazelinia <utilyre@proton.me>
2023-04-26 22:04:29 +0330

    refactor(cursor): move Cursor and its things to matrix::cursor

---
+++ src/cursor.rs
@@ -0,0 +1,51 @@
+pub struct Cursor<I> {
+    iter: I,
+
+    width: usize,
+    i: usize,
+    j: usize,
+}
+
+impl<I> Cursor<I> {
+    fn new(iter: I, width: usize) -> Self {
+        Self {
+            iter,
+            width,
+            i: 0,
+            j: 0,
+        }
+    }
+}
+
+impl<I> Iterator for Cursor<I>
+where
+    I: Iterator,
+{
+    type Item = ((usize, usize), I::Item);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let a = self.iter.next();
+
+        let i = self.i;
+        let j = self.j;
+
+        self.j += 1;
+        if self.j == self.width {
+            self.i += 1;
+            self.j = 0;
+        }
+
+        a.map(|a| ((i, j), a))
+    }
+}
+
+pub trait CursorIter<T> {
+    fn cursor(self, width: usize) -> Cursor<Self>
+    where
+        Self: Sized + Iterator<Item = T>,
+    {
+        Cursor::new(self, width)
+    }
+}
+
+impl<I, T> CursorIter<T> for I where I: Iterator<Item = T> {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,8 @@
 use std::slice::Iter;
 
-#[derive(Clone)]
-pub struct Cursor<I> {
-    iter: I,
+use cursor::{Cursor, CursorIter};
 
-    width: usize,
-    i: usize,
-    j: usize,
-}
-
-impl<I> Cursor<I> {
-    fn new(iter: I, width: usize) -> Self {
-        Self {
-            iter,
-            width,
-            i: 0,
-            j: 0,
-        }
-    }
-}
-
-impl<I> Iterator for Cursor<I>
-where
-    I: Iterator,
-{
-    type Item = ((usize, usize), I::Item);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let a = self.iter.next();
-
-        let i = self.i;
-        let j = self.j;
-
-        self.j += 1;
-        if self.j == self.width {
-            self.i += 1;
-            self.j = 0;
-        }
-
-        a.map(|a| ((i, j), a))
-    }
-}
-
-trait CursorIter<T> {
-    fn cursor(self, width: usize) -> Cursor<Self>
-    where
-        Self: Sized + Iterator<Item = T>,
-    {
-        Cursor::new(self, width)
-    }
-}
-
-impl<I, T> CursorIter<T> for I where I: Iterator<Item = T> {}
+mod cursor;
 
 pub struct Matrix<T> {
     width: usize,
